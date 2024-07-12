a231241c66b6024172bd487e1058429b40103ba5
tiennt <tiendo1242.dev@gmail.com>
2024-07-12 14:03:14 +0700

    oke

--- src/modules/auth/guards/upload.guard.ts
+++ src/modules/auth/guards/upload.guard.ts
@@ -20,10 +20,10 @@ import { Role } from 'src/entities/account.entity';
         const controller = context.getClass();
         const endpoint = `${controller.name}.${handler.name}`;
         this.logger.log(`Accessing endpoint: ${endpoint}`);
-        if (user?.role !== Role.admin && user?.role !== Role.staff) {
-          return false;
+        if (user?.role == Role.admin || user?.role == Role.staff) {
+          return true;
         }
-        return true;
+        return false;
       } catch (error) {
         return false;
       }
