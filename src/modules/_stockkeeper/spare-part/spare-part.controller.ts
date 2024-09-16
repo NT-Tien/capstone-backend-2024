c772caedac54d3cd6db9b485ccaef2a0f2e5f9f1
tiennt <tiendo1242.dev@gmail.com>
2024-09-16 22:37:27 +0700

    update

--- src/modules/_stockkeeper/spare-part/spare-part.controller.ts
+++ src/modules/_stockkeeper/spare-part/spare-part.controller.ts
@@ -63,7 +63,7 @@ export class SparePartController {
     @Param('id') id: string,
     @Body() body: SparePartRequestDto.SparePartUpdateDto,
   ) {
-    return await this.sparePartService.update(
+    return await this.sparePartService.customUpdate(
       id,
       SparePartRequestDto.SparePartUpdateDto.plainToClass(body),
     );
