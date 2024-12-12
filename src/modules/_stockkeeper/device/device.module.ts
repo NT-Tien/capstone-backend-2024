2944b297584f3d1dd81cde8f8beb3b1bb81fe991
tiennt <tiendo1242.dev@gmail.com>
2024-12-12 21:41:10 +0700

    update

--- src/modules/_stockkeeper/device/device.module.ts
+++ src/modules/_stockkeeper/device/device.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AreaEntity } from 'src/entities/area.entity';
 import { DeviceEntity } from 'src/entities/device.entity';
+import { TaskEntity } from 'src/entities/task.entity';
 import { DeviceController } from 'src/modules/_stockkeeper/device/device.controller';
 import { DeviceService } from 'src/modules/_stockkeeper/device/device.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DeviceEntity, AreaEntity])],
+  imports: [TypeOrmModule.forFeature([DeviceEntity, AreaEntity, TaskEntity])],
   controllers: [DeviceController],
   providers: [DeviceService],
 })
